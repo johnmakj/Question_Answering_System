7bb51537213a7b7a28e1d15972a007c81f3db987
jmakris <jmakris@pipacorp.com>
2024-02-04 18:09:53 +0200

    Add demo website

--- src/machine_learning/training/bert_qa_layer.py
+++ src/machine_learning/training/bert_qa_layer.py
@@ -40,6 +40,7 @@ class CustomQuestionAnsweringModel(BertPreTrainedModel):
         end_logits = end_logits.squeeze(-1)
 
         loss = None
+        """
         if start_positions is not None and end_positions is not None:
             start_positions = torch.clamp(start_positions, 0, input_ids.size(1) - 1)
             end_positions = torch.clamp(end_positions, 0, input_ids.size(1) - 1)
@@ -48,6 +49,25 @@ class CustomQuestionAnsweringModel(BertPreTrainedModel):
             start_loss = loss_fct(start_logits, start_positions)
             end_loss = loss_fct(end_logits, end_positions)
             loss = (start_loss + end_loss) / 2
+        """
+
+        if start_positions is not None and end_positions is not None:
+            if (start_positions >= 0).all() and (end_positions < input_ids.size(1)).all():
+                # Continue with the calculation
+                loss_fct = nn.CrossEntropyLoss()
+                start_loss = loss_fct(start_logits, start_positions)
+                end_loss = loss_fct(end_logits, end_positions)
+                loss = (start_loss + end_loss) / 2
+            else:
+                # Handle the case where start_positions or end_positions are out of range
+                start_positions = torch.clamp(start_positions, 0, input_ids.size(1) - 1)
+                end_positions = torch.clamp(end_positions, 0, input_ids.size(1) - 1)
+                # If start and end positions are provided, calculate the loss
+                loss_fct = nn.CrossEntropyLoss()
+                start_loss = loss_fct(start_logits, start_positions)
+                end_loss = loss_fct(end_logits, end_positions)
+                loss = (start_loss + end_loss) / 2
+                # Optionally, you can set loss to some default value or take other actions.
 
         # Return logits along with the loss during training
         if self.training:
